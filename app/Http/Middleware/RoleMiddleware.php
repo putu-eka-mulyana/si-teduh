8892b314d070f376893ab5e1ef26340af1db1008
putu eka mulyana <putuekamulyana97@gmail.com>
2025-07-27 11:42:37 +0800

    Implement Role-Based Access Control pada middleware dan routes untuk admin dan user

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
@@ -13,8 +14,15 @@ class RoleMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+        if (!in_array(Auth::user()->role, $roles)) {
+            abort(403, 'Akses ditolak');
+        }
+
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,40 +25,44 @@ Route::get('/beranda', function () {
 
 // admin routes
 
-Route::prefix('admin')->group(function () {
-    Route::get('/', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
-    Route::get('/data-pasien', [PatientController::class, 'index'])->name('admin.list-patient');
-    Route::get('/data-pasien/tambah', [AuthController::class, 'showRegisterPatientForm'])->name('admin.add-patient');
-    Route::post('/data-pasien/tambah', [AuthController::class, 'register'])->name('admin.store-patient');
-    Route::delete('/data-pasien/{id}', [PatientController::class, 'destroy'])->name('admin.delete-patient');
-    Route::get('/data-pasien/edit/{id}', [PatientController::class, 'edit'])->name('admin.edit-patient');
-    Route::post('/data-pasien/edit/{id}', [PatientController::class, 'update'])->name('admin.update-patient');
+Route::middleware('role:ADMIN')->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('/', function () {
+            return view('admin.dashboard');
+        })->name('admin.dashboard');
+        Route::get('/data-pasien', [PatientController::class, 'index'])->name('admin.list-patient');
+        Route::get('/data-pasien/tambah', [AuthController::class, 'showRegisterPatientForm'])->name('admin.add-patient');
+        Route::post('/data-pasien/tambah', [AuthController::class, 'register'])->name('admin.store-patient');
+        Route::delete('/data-pasien/{id}', [PatientController::class, 'destroy'])->name('admin.delete-patient');
+        Route::get('/data-pasien/edit/{id}', [PatientController::class, 'edit'])->name('admin.edit-patient');
+        Route::post('/data-pasien/edit/{id}', [PatientController::class, 'update'])->name('admin.update-patient');
 
-    // page scheduling
-    Route::get('/data-jadwal', [ScheduleController::class, 'index'])->name('admin.list-schedule');
-    Route::get('/data-jadwal/tambah', function () {
-        return view('admin.add-schedule');
-    })->name('admin.add-schedule');
-    Route::post('/data-jadwal/tambah', [ScheduleController::class, 'store'])->name('admin.store-schedule');
+        // page scheduling
+        Route::get('/data-jadwal', [ScheduleController::class, 'index'])->name('admin.list-schedule');
+        Route::get('/data-jadwal/tambah', function () {
+            return view('admin.add-schedule');
+        })->name('admin.add-schedule');
+        Route::post('/data-jadwal/tambah', [ScheduleController::class, 'store'])->name('admin.store-schedule');
 
-    Route::get('/data-jadwal/edit/{id}', function ($id) {
-        return view('admin.edit-schedule', ['id' => $id]);
-    })->name('admin.edit-schedule');
-    Route::put('/data-jadwal/edit/{id}', [ScheduleController::class, 'update'])->name('admin.update-schedule');
+        Route::get('/data-jadwal/edit/{id}', function ($id) {
+            return view('admin.edit-schedule', ['id' => $id]);
+        })->name('admin.edit-schedule');
+        Route::put('/data-jadwal/edit/{id}', [ScheduleController::class, 'update'])->name('admin.update-schedule');
 
-    Route::get('/list', [AdminController::class, 'index'])->name('admin.list');
-    Route::post('/add', [AuthController::class, 'register'])->name('admin.store');
-    Route::delete("/delete/{id}", [AdminController::class, 'destroy'])->name("admin.destroy");
-    Route::put('/edit/{id}', [AdminController::class, 'update'])->name('admin.update');
+        Route::get('/list', [AdminController::class, 'index'])->name('admin.list');
+        Route::post('/add', [AuthController::class, 'register'])->name('admin.store');
+        Route::delete("/delete/{id}", [AdminController::class, 'destroy'])->name("admin.destroy");
+        Route::put('/edit/{id}', [AdminController::class, 'update'])->name('admin.update');
+    });
 });
 
 
 
 // buat route untuk handle user view
-Route::prefix('user')->group(function () {
-    Route::get('/', function () {
-        return view('user-view');
-    })->name('user-view');
+Route::middleware('role:USER')->group(function () {
+    Route::prefix('user')->group(function () {
+        Route::get('/', function () {
+            return view('user-view');
+        })->name('user-view');
+    });
 });
